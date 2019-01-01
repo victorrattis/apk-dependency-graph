5f8d8f4753fbad02a1bbbf9f0b0bca78abb2a306
Victor Rattis <victorrattis@gmail.com>
2019-01-01 19:04:49 -0300

    Rename lib and app project names
     - Change name from apk-dependecy-graph-lib to
     apk-dependency-visualizer.
     - Change name from gui to app.

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
-cd apk-dependecy-graph-lib
+cd apk-dependecy-analyzer
 ant
 cd ..
 
-LIB_FILE=apk-dependecy-graph-lib/build/jar/apk-dependency-graph.jar
+LIB_FILE=apk-dependecy-analyzer/build/jar/apk-dependency-graph.jar
 if [ -f "$LIB_FILE" ]; then
-    mv "$LIB_FILE" gui/lib/
-    echo "done"
+    mv "$LIB_FILE" app/lib/ 
 else 
-    echo "apk-dependency-graph.jar does not exist!"
+    echo "apk-dependency-graph.jar not found!"
 fi
